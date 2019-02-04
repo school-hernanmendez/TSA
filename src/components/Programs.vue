3a1c360ec7384ce9b0e52f5b8db0c5b73e70a3bd
YO <0820967@student.osceolaschools.net>
2019-02-04 10:38:29 -0500

    g

--- src/components/Programs.vue
+++ src/components/Programs.vue
@@ -3,6 +3,34 @@
     <div class="program full">
 
     </div>
+    <div class="apc">
+      <h3>AP Computer Science Principles</h3>
+      <p></p>
+    </div>
+    <div class="dit">
+      <h3>Digital Information Technology</h3>
+      <p></p>
+    </div>
+    <div class="game">
+      <h3>Game and Simulation Foundations</h3>
+      <p></p>
+    </div>
+    <div class="game2">
+      <h3>Game and Simulation Design</h3>
+      <p></p>
+    </div>
+    <div class="dmt">
+      <h3>Digital Media/Multi Media Foundations</h3>
+      <p></p>
+    </div>
+    <div class="tv">
+      <h3>Digital Video Technology - Television Production</h3>
+      <p></p>
+    </div>
+    <div class="dai">
+      <h3>Digital Art Imaging</h3>
+      <p></p>
+    </div>
   </div>
 </template>
 
