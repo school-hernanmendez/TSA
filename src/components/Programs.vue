15cd07c4c8444574a9a7ee113a060f8551a1dafc
Hernan Mendez <hernan2111200@gmail.com>
2019-02-05 00:54:31 -0500

    det

--- src/components/Programs.vue
+++ src/components/Programs.vue
@@ -6,7 +6,11 @@
       <div>
         <h3>What is CTE?</h3>
         <p>Career and Technical Education (CTE) gives high school students the chance to get a head start on preparing for college and careers. In CTE programs you will learn how core school subjects like math, science and writing are used in real-life. As a CTE student you have the opportunity to participate in hands-on training in your chosen program and gain real world experience through job shadows and internships. Many programs offer you the opportunity to earn nationally recognized certifications which you can use to get a job, that will help you pay for college, or start off your career straight out of high school!</p>
-        <span>Source: <a href="https://chooseyourfuture.cps.edu/career-technical-education/what-is-cte/">https://chooseyourfuture.cps.edu/career-technical-education/what-is-cte/</a></span>
+        <span>Source:
+          <a
+            href="https://chooseyourfuture.cps.edu/career-technical-education/what-is-cte/"
+          >https://chooseyourfuture.cps.edu/career-technical-education/what-is-cte/</a>
+        </span>
         <p class="CTE">Here are the CTE programs our school offers:</p>
       </div>
     </div>
@@ -14,86 +18,58 @@
       <h2>Technology</h2>
       <div class="teche left">
         <div>
-        <h3>AP Computer Science Principles</h3>
-        <p></p>
+          <h3>AP Computer Science Principles</h3>
+          <p></p>
         </div>
         <div class="img"></div>
       </div>
       <div class="teche right">
         <div>
-        <h3>Digital Information Technology</h3>
-        <p></p>
+          <h3>Digital Information Technology</h3>
+          <p></p>
         </div>
         <div class="img"></div>
       </div>
       <div class="teche left">
         <div>
-        <h3>Game and Simulation Foundations/Design</h3>
-        <p></p>
+          <h3>Game and Simulation Foundations/Design</h3>
+          <p></p>
         </div>
         <div class="img"></div>
       </div>
       <div class="teche right">
         <div>
-        <h3>Digital Video Technology</h3>
-        <p></p>
+          <h3>Digital Video Technology</h3>
+          <p></p>
         </div>
         <div class="img"></div>
       </div>
       <div class="teche right">
         <div>
-        <h3>Television Production</h3>
-        <p></p>
+          <h3>Television Production</h3>
+          <p></p>
         </div>
         <div class="img"></div>
       </div>
     </div>
     <div class="arts">
       <h2>Arts</h2>
-      <div class="art ">
+      <div class="art">
         <div class="art left">
-        <div>
-        <h3>Digital Media/Multi Media Foundations</h3>
-        <p></p>
+          <div>
+            <h3>Digital Media/Multi Media Foundations</h3>
+            <p></p>
+          </div>
+          <div class="img">Digital Art Imaging</div>
         </div>
-        <div class="img">Digital Art Imaging</div>
-      </div>
-      <div class="art right">
-        <div>
-        <h3></h3>
-        <p></p>
+        <div class="art right">
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+          <div class="img"></div>
         </div>
-        <div class="img"></div>
       </div>
-      </div>
-    </div>
-    <div class="apc">
-      <h3>AP Computer Science Principles</h3>
-      <p></p>
-    </div>
-    <div class="dit">
-      <h3>Digital Information Technology</h3>
-      <p></p>
-    </div>
-    <div class="game">
-      <h3>Game and Simulation Foundations</h3>
-      <p></p>
-    </div>
-    <div class="game2">
-      <h3>Game and Simulation Design</h3>
-      <p></p>
-    </div>
-    <div class="dmt">
-      <h3>Digital Media/Multi Media Foundations</h3>
-      <p></p>
-    </div>
-    <div class="tv">
-      <h3>Digital Video Technology - Television Production</h3>
-      <p></p>
-    </div>
-    <div class="dai">
-      <h3>Digital Art Imaging</h3>
-      <p></p>
     </div>
   </div>
 </template>
@@ -130,7 +106,8 @@ export default {
 .tech {
   background: url("../assets/u-astronaut.jpg");
 }
-.arts h2, .tech h2 {
+.arts h2,
+.tech h2 {
   font-size: 60px;
   color: white;
   margin: 0;
@@ -143,6 +120,6 @@ export default {
   font-family: "Righteous", cursive;
 }
 .arts h2 {
-  font-family: 'Rock Salt', cursive;
+  font-family: "Rock Salt", cursive;
 }
 </style>
